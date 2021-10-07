29c89bd19373c05aa58335d761141308c6ed1360
Lucas <lucaswillian45@gmail.com>
2021-10-07 14:19:23 -0300

    feat: create SizeWithComposedMeasure

---
+++ src/main/java/sizebay/catalog/client/model/SizeWithComposedMeasure.java
@@ -0,0 +1,13 @@
+package sizebay.catalog.client.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SizeWithComposedMeasure {
+
+	private String sizeName;
+	private String composedMeasureValue;
+
+}
