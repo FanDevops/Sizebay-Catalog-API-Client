afb69e0b7106310aac265898308dfacd05c550e7
gabrisamartinez <gabrielly_martinez@hotmail.com>
2021-10-19 15:29:42 -0300

    feat: added sku on product

--- src/main/java/sizebay/catalog/client/model/Product.java
+++ src/main/java/sizebay/catalog/client/model/Product.java
@@ -58,6 +58,8 @@ public class Product   {
   private List<SizeWithComposedMeasure> availableSizesWithComposedMeasure;
   private ImportationError importationError;
 
+  private List<String> skus;
+
   @RequiredArgsConstructor
   public enum AgeGroupEnum {
     ADULT("adult"),
