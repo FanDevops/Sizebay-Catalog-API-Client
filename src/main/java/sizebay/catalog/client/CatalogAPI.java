a959cf4778bd5051b6a347e2fdf3740027f10dca
Thiago <thiagogonc4@gmail.com>
2021-11-16 14:28:15 -0300

    feat: added method to updated user, user permissions and create user

--- src/main/java/sizebay/catalog/client/CatalogAPI.java
+++ src/main/java/sizebay/catalog/client/CatalogAPI.java
@@ -841,12 +841,24 @@ public class CatalogAPI {
 	 * Starting my sizebay user management
 	 */
 
+	public void createMySizebayUser(String username, MySizebayUser user) {
+		client.post("/users/admin/" + username, user);
+	}
+
 	public MySizebayUser getMySizebayUser(String username){
 		return client.getSingle( "/users/" + username, MySizebayUser.class);
 	}
 
 	public Long insertMySizebayUser(MySizebayUser user){
 		return client.post("/users/", user);
+	}
+
+	public void updateMySizebayUser(String adminUsername, String userUsername, MySizebayUser user) {
+		client.put("/tenants/users/update/" + adminUsername + "/" + userUsername, user);
+	}
+
+	public void updateMySizebayUserPermissions(String adminUsername, String userUsername, List<Long> tenantsId) {
+		client.post("/users/update/permissions/" + adminUsername + "/" + userUsername, tenantsId);
 	}
 
 	/*
