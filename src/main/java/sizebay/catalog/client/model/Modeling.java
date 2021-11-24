d3600fec0a124e901c7e1bf09b7eddf72f2b9b02
Thiago <thiagogonc4@gmail.com>
2021-11-24 15:38:32 -0300

    feat: added modelingCategory to modeling

--- src/main/java/sizebay/catalog/client/model/Modeling.java
+++ src/main/java/sizebay/catalog/client/model/Modeling.java
@@ -15,24 +15,24 @@ public class Modeling   {
   private String gMerchantBrandName;
   private String gMerchantAgeGroup;
   private String gMerchantSizeType;
-	private String observation;
+  private String observation;
 
   private TypeEnum type;
   private Long brandId;
   private Measure composedMeasure;
   private ComposedMeasureOrder composedMeasureOrder;
-	private SizeSystem sizeSystem;
+  private SizeSystem sizeSystem;
   private List<ModelingSizeMeasures> measures = new ArrayList<>();
-	private List<CustomRange> customRanges = new ArrayList<>();
+  private List<CustomRange> customRanges = new ArrayList<>();
 
-	private Long originalModelingId;
+  private Long originalModelingId;
 
   private StrongCategoryType strongCategoryType;
 
-	boolean active = true;
-	boolean accessories = false;
+  boolean active = true;
+  boolean accessories = false;
 
-  private List<Long> categoriesIds = new ArrayList<Long>();
+  private List<ModelingCategory> modelingCategories = new ArrayList<ModelingCategory>();
 
   private Boolean isShoeModeling;
 
---
+++ src/main/java/sizebay/catalog/client/model/ModelingCategory.java
@@ -0,0 +1,14 @@
+package sizebay.catalog.client.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ModelingCategory {
+
+    long modelingId;
+    long categoryId;
+    String categoryName;
+
+}
